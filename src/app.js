991f448c2e36d548dfa6273a71adb4462e97b85e
Vijay krishna Nimmana <vijaykrishnanimmana@Vijays-MacBook-Pro.local>
2025-09-03 23:19:10 +0530

    app.use understanding the wild card route

--- src/app.js
+++ src/app.js
@@ -1,33 +1,35 @@
 const express = require('express');
 const app = express();
 
-// app.use( "/", (req, res) => {
-//     res.send("Hello from Empty");
-// });
-// app.use('/test', (req, res) => {
-//     res.send("Hello i'm from the Testserver");
-// });
-// app.use('/data', (req, res) => {
-//     res.send("Hello i'm from the Dataserver");
-// });
+app.use('/test', (req, res) => {
+    res.send("Hello i'm from the Testserver");
+});
 
-app.get("/", (req,res) => {
-    res. send ("Hello from Vijay!");
- });
+app.use('/data', (req, res) => {
+    res.send("Hello i'm from the Dataserver");
+});
 
-app.get("/hello", (req,res) => {
-    res. send ("Hello hello hello!");
- });
+app.use( "/", (req, res) => {
+    res.send("Hello from Empty");
+});
 
-app.get("/bolo", (req,res) => {
-    res. send ("Bolo Bhai!");
- });
-app.get("/userDeatils", (req,res) => {
-    res. send ("User Details");
- });
-app.get("/Deatils", (req,res) => {
-    res. send ("Just Details");
- });
+// app.get("/", (req,res) => {
+//     res. send ("Hello from Vijay!");
+//  });
+
+// app.get("/hello", (req,res) => {
+//     res. send ("Hello hello hello!");
+//  });
+
+// app.get("/bolo", (req,res) => {
+//     res. send ("Bolo Bhai!");
+//  });
+// app.get("/userDeatils", (req,res) => {
+//     res. send ("User Details");
+//  });
+// app.get("/Deatils", (req,res) => {
+//     res. send ("Just Details");
+//  });
 
 app.listen(7777, () => {
     console.log("listening on port 7777");
